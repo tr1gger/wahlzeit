8d0561fa1072d0d5a9be138cea0c946938bcbbea
robert miller <robert.miller@fau.de>
2016-11-13 21:30:52 +0100

    remove createPhoto method; super class will be called instead

--- src/main/java/org/wahlzeit/model/CarPhotoManager.java
+++ src/main/java/org/wahlzeit/model/CarPhotoManager.java
@@ -1,39 +1,12 @@
 package org.wahlzeit.model;
 
-
-import com.google.appengine.api.images.Image;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class CarPhotoManager extends PhotoManager {
 
-    /**
-     *
-     */
     protected static final PhotoManager instance = new CarPhotoManager();
 
-    /**
-     *
-     */
     protected Map<PhotoId, Photo> photos = new HashMap<PhotoId, Photo>();
-
-    public CarPhotoManager(){
-        super();
-    }
-
-    public PhotoManager getCarPhotoManager(){
-        return instance;
-    }
-
-    /**
-     *
-     */
-    public Photo createPhoto(String filename, Image uploadedImage) throws Exception {
-        PhotoId id = PhotoId.getNextId();
-        Photo result = PhotoUtil.createCarPhoto(filename, id, uploadedImage);
-        addPhoto(result);
-        return result;
-    }
 
 }
