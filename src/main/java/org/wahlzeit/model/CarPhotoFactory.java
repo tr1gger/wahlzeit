10d5720cbec6ee964b3cf5db9d963db7e5d58137
robert miller <robert.miller@fau.de>
2016-11-13 21:30:15 +0100

    add  instance setter and Override Tags

--- src/main/java/org/wahlzeit/model/CarPhotoFactory.java
+++ src/main/java/org/wahlzeit/model/CarPhotoFactory.java
@@ -19,10 +19,23 @@ public class CarPhotoFactory extends PhotoFactory {
         return instance;
     }
 
+    /**
+     * Method to set the singleton instance of PhotoFactory.
+     */
+    protected static synchronized void setInstance(PhotoFactory photoFactory) {
+        if (instance != null) {
+            throw new IllegalStateException("attempt to initalize PhotoFactory twice");
+        }
+
+        instance = photoFactory;
+    }
+
+    @Override
     public Photo createPhoto(){
          return new CarPhoto();
     }
 
+    @Override
     public Photo createPhoto(PhotoId id){
         return new CarPhoto(id);
     }
