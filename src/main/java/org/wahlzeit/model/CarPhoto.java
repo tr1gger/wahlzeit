cc06b730dcea80b3acc876751f2cb508b2579a39
robert miller <robert.miller@fau.de>
2016-11-13 21:41:19 +0100

    remove logger

--- src/main/java/org/wahlzeit/model/CarPhoto.java
+++ src/main/java/org/wahlzeit/model/CarPhoto.java
@@ -1,16 +1,10 @@
 package org.wahlzeit.model;
 
-
 import com.googlecode.objectify.annotation.Subclass;
 
-import java.util.logging.Logger;
 
 @Subclass
 public class CarPhoto extends Photo {
-
-
-    private static final Logger log = Logger.getLogger(CarPhoto.class.getName());
-
     public CarPhoto(){
         super();
     }
