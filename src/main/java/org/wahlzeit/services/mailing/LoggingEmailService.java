56c14c35d4a209ad32f71a4d5611d662f9c2cf05
robert miller <robert.miller@fau.de>
2017-01-05 15:26:44 +0100

    Decorator Patten

--- src/main/java/org/wahlzeit/services/mailing/LoggingEmailService.java
+++ src/main/java/org/wahlzeit/services/mailing/LoggingEmailService.java
@@ -20,6 +20,7 @@
 
 package org.wahlzeit.services.mailing;
 
+import org.wahlzeit.annotation.PatternInstance;
 import org.wahlzeit.services.EmailAddress;
 import org.wahlzeit.services.LogBuilder;
 
@@ -28,6 +29,10 @@ import java.util.logging.Logger;
 /**
  * A logging mailing service logs email send attempts before sending emails. This is a decorator pattern application.
  */
+@PatternInstance(
+		patternName = "Decorator",
+		participants = {"Component", "ConcreteDecorator"}
+)
 public class LoggingEmailService implements EmailService {
 
 	private static final Logger log = Logger.getLogger(LoggingEmailService.class.getName());
